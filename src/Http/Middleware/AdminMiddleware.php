7cba413951cff95699157846338e64b6a9d17a94
Michel Melo <michellander@gmail.com>
2025-07-17 14:03:14 +0100

    fix: Update error message for unauthenticated access in AdminMiddleware

--- src/Http/Middleware/AdminMiddleware.php
+++ src/Http/Middleware/AdminMiddleware.php
@@ -22,7 +22,7 @@ class AdminMiddleware
         if (EnsureFrontendRequestsAreStateful::fromFrontend($request)) {
             if (! auth('admin')->user()) {
                 return response([
-                    'message' => __('rest-api::app.common.forbidden-error'),
+                    'message' => __('rest-api::app.common.unauthenticated'),
                 ], 401);
             }
 
@@ -37,7 +37,7 @@ class AdminMiddleware
         }
 
         return response([
-            'message' => __('rest-api::app.common.forbidden-error'),
+            'message' => __('rest-api::app.common.unauthenticated'),
         ], 401);
     }
 }
